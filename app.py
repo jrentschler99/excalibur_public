94680b41cb08c9cd203f8cd29eca08943ffce37c
Jim Rentschler <jrentschler123@gmail.com>
2021-06-08 19:38:10 -0400

    Add api permissions

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ try:
     headers = {'Content-Type': 'application/repository.folder+json'}
     resource = {"uri": "/Jim", "label": "Jim",
                 "description": "Demo folder", "permissionMask": "0",
-                "creationDate": "2021-06-08T12:00:0", "updateDate": "2021-06-04T12:00:0", "version": "6"}
+                "creationDate": "2021-06-08T12:00:0", "updateDate": "2021-06-04T12:00:0", "version": "0"}
     put_folder = jasper_session.put(url=folder_address,
                                     json=resource,
                                     auth=(JASPER_USERNAME,JASPER_PASSWORD),
@@ -62,41 +62,31 @@ except Exception as e:
     print("An error occurred while trying to create this resource: " + str(e))
 
 
-# Create test user
+
+# Apply read, write, delete access to new folder
 try:
     jasper_session = requests.session()
     jasper_session.verify = False  # https://stackoverflow.com/questions/15445981/how-do-i-disable-the-security-certificate-check-in-python-requests
-    user_address = JASPER_ADDRESS + "/rest_v2/users/TestUser001"
+    permission_address = JASPER_ADDRESS + "/rest_v2/permissions"
     headers = {'Content-Type': 'application/json'}
     resource = {
-        "fullName": "TestUser001",
-        "emailAddress": "jrentschler123@gmail.com",
-        "enabled": "true",
-        "password": "password",
-        "roles": [
-            {"name": "ROLE_ADMINISTRATOR"}]
+        "uri": "/Jim",
+        "recipient": "user:/TestUser001",
+        "mask": "30" # Mask defines what level permission https://community.jaspersoft.com/wiki/repository-resource-permission
     }
-    put_user = jasper_session.put(url=user_address,
+    post_permission = jasper_session.post(url=permission_address,
                                     json=resource,
                                     auth=(JASPER_USERNAME, JASPER_PASSWORD),
                                     headers=headers)
-    if (put_user.status_code == 200):
-        print("User appears to have been created correctly.")
+    if (post_permission.status_code == 201):
+        print("Permission appears to have been added correctly.")
     else:
-        print("An error occurred: " + str(put_user.text))
-        put_user.raise_for_status()
+        print("An error occurred: " + str(post_permission.text))
+        post_permission.raise_for_status()
 except Exception as e:
-    print("An error occurred while trying to create this resource: " + str(e))
+    print("An error occurred while trying to add this resource: " + str(e))
 
 
-def put_resource(JASPER_USERNAME,JASPER_PASSWORD, JASPER_ADDRESS, resource, headers, resource_address):
-    jasper_session = requests.session()
-    jasper_session.verify = False # https://stackoverflow.com/questions/15445981/how-do-i-disable-the-security-certificate-check-in-python-requests
-    address = JASPER_ADDRESS + resource_address
-    response = jasper_session.put(url=user_address,
-                                    json=resource,
-                                    auth=(JASPER_USERNAME, JASPER_PASSWORD),
-                                    headers=headers)
 
 
 
